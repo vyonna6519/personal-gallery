ced9a6b3dc3af9a80d36d39e32b4b78d181aa710
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-27 17:40:14 +0300

    added testclass

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from .models import Image,Location,Category
 
-# Create your tests here.
+
+class GalleryTestClass(TestCase):
+    def setUp(self):
+        self.new_category = Category(category_name='Music')
+        self.new_category.save_category()
+        self.new_location = Location(location_name = 'Italy')
+        self.new_location.save_location()
+        self.new_image = Image(id=1,image_name='violin', image_description='violin',image_path='media/gallery/violin.jpg',image_category=self.new_category,image_location=self.new_location)
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_is_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
+        self.assertTrue(isinstance(self.new_category,Category))
+        self.assertTrue(isinstance(self.new_location,Location))
+
+    def test_save_method(self):
+        self.new_image.save_image()
+        all_objects = Image.objects.all()
+        self.assertTrue(len(all_objects)>0)
+
+    def test_delete_method(self):
+        self.new_image.save_image()
+        filtered_object = Image.objects.filter(image_name='violin')
+        Image.delete_image(filtered_object)
+        all_objects = Image.objects.all()
+        self.assertTrue(len(all_objects) == 0)
+
+    def test_display_all_objects_method(self):
+        self.new_image.save_image()
+        all_objects = Image.retrieve_all()
+        self.assertEqual(all_objects.get(id=1).image_name,'violin')
+
+
+    def test_update_single_object_property(self):
+        self.new_image.save_image()
+        filtered_object =Image.update_image('violin','Greener')
+        fetched = Image.objects.get(image_name='Greener')
+        self.assertEqual(fetched.get(id=1).image_name,'Greener')
+    def test_get_image_by_id(self):
+        self.new_image.save_image()
+        fetched_image = Image.get_image_by_id(1)
+        self.assertEqual(fetched_image.id,1)
+    def test_search_by_category(self):
+        self.new_image.save_image()
+        fetch_specific = Category.objects.get(category_name='Music')
+        self.assertTrue(fetch_specific.category_name=='Music')
+    def test_filter_by_location(self):
+        self.new_image.save_image()
+        fetch_specific = Image.filter_location('Italy')
+        self.assertEqual(fetch_specific.get(id=1),self.new_image)
