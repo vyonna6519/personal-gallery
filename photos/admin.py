1671eb55a63e8aeb44ae5b01e38b40421185c836
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-27 17:40:50 +0300

    registered my models

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,7 @@
+from photos.models import Category, Image, Location
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
