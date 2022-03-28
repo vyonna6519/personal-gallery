4d870d000e264bdcaef2aab484f29e273804d295
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-28 09:39:35 +0300

    added images to app

--- photos/urls.py
+++ photos/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('',views.landing,name='landing'),
-    path('search',views.search_results, name='search_results'),
+    path('searchz',views.search_results, name='search_results'),
     path('category', views.page_category,name='page_category'),
     path('location', views.page_location,name='page_location'),
 ]
